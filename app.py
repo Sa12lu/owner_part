1b7578417f8452adb2552e1703e6ce4939d72c27
Nur Fatihah Ismail <saluhahsofia@gmail.com>
2025-05-10 00:13:45 +0800

    login

--- app.py
+++ app.py
@@ -29,6 +29,65 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+hashed = generate_password_hash('admin123', method='pbkdf2:sha256')
+print(hashed)
+
+# Database Models
+class Owner(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+class BuyStock(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    datetime = db.Column(db.DateTime, default=datetime.utcnow)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        owner_user = Owner.query.filter_by(username=username).first()
+
+        if owner_user and check_password_hash(owner_user.password, password):
+            session['username'] = username
+            flash('Login successful!', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Invalid credentials.', 'danger')
+
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'username' in session:
+        return render_template('dashboard.html', username=session['username'])
+    else:
+        flash('You need to log in first.', 'warning')
+        return redirect(url_for('login'))
+
+@app.route('/buy-stock')
+def buy_stock():
+    BuyStock = BuyStock.query.all()
+    return render_template('buy_stock.html', BuyStock = BuyStock)
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    flash('You have been logged out.', 'info')
+    return redirect(url_for('login'))
+
 if __name__ == "__main__":
     print("Starting Flask app...")
     app.run(host="0.0.0.0", port=5002, debug=True)
